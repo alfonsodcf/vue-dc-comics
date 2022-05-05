27c9c21301217eb9357bf590d956cd1c31d25d65
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-05 21:09:16 +0200

    comics

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    
+    <app-header/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Prova</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+ 
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
