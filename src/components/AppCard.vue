9fa3f93592e6ee521ce2cb7556b2377c792c8880
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-11 12:15:16 +0200

    AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+        <!-- <img :src="detail.thumb" :alt="detail.series">
+        <h3>{{detail.series}}</h3> -->
+        <p>prova</p> 
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppCard',
+        data(){
+            return{
+
+            }
+        },
+        props: {
+            datail: Object
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
